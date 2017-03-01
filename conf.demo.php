019b2b588eceef83ae0d23220806046c91b9f9c8
Pedro <pjamorim91@gmail.com>
2017-03-01 15:24:46 -0100

    Added option to show deleted objects if versioning was enabled at the time of deletion. This closes #2

--- conf.demo.php
+++ conf.demo.php
@@ -6,3 +6,6 @@ define('CLIENT_ID', 'your-client-id');
 define('CLIENT_SECRET', 'your-client-secret');
 define('REDIRECT_URI', 'your-redirect-uri');
 
+#This should only be considered if objects have been deleted after versioning has been set on the bucket
+define('SHOW_DELETED_ITEMS', false);
+
